d4a25a4f905a0260edb04ea9030889a4d2938157
hashbury1 <efefiomarchibong@gmail.com.com>
2026-01-12 18:45:22 +0100

    fixing on main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -24,17 +24,17 @@ resource "aws_ecr_repository" "api" {
 # VPC
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
-  tags = { 
-    Name = "portfolio-vpc" 
-    }
+  tags = {
+    Name = "portfolio-vpc"
+  }
 }
 
 resource "aws_subnet" "public_a" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
-  tags = { 
-    Name = "public-a" 
+  tags = {
+    Name = "public-a"
   }
 }
 
@@ -42,9 +42,9 @@ resource "aws_subnet" "public_b" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1b"
-  tags = { 
-    Name = "public-b" 
-    }
+  tags = {
+    Name = "public-b"
+  }
 }
 
 resource "aws_internet_gateway" "main" {
@@ -59,52 +59,54 @@ resource "aws_route_table" "public" {
   }
 }
 
-resource "aws_route_table_association" "a" { 
-    subnet_id = aws_subnet.public_a.id 
-    route_table_id = aws_route_table.public.id 
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
 }
 
-resource "aws_route_table_association" "b" { 
-    subnet_id = aws_subnet.public_b.id 
-    route_table_id = aws_route_table.public.id 
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public.id
 }
 
 # Security Group
 resource "aws_security_group" "alb" {
   vpc_id = aws_vpc.main.id
-  ingress { 
-    from_port = 80 
-  to_port = 80; protocol = "tcp" 
-  cidr_blocks = ["0.0.0.0/0"] 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress { 
-    from_port = 0 
-    to_port = 0 
-    protocol = "-1" 
-    cidr_blocks = ["0.0.0.0/0"] 
-    }
-  tags = { 
-    Name = "alb-sg" 
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "alb-sg"
+  }
 }
 
 resource "aws_security_group" "ecs" {
   vpc_id = aws_vpc.main.id
-  ingress { 
-    from_port = 8080 
-    to_port = 8080 
-  protocol = "tcp" 
-  security_groups = [aws_security_group.alb.id] 
+  ingress {
+    from_port       = 8080
+    to_port         = 8080
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb.id]
   }
 
-  egress { 
-    from_port = 0; to_port = 0 
-    protocol = "-1" 
-    cidr_blocks = ["0.0.0.0/0"] 
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-  tags = { 
-    Name = "ecs-sg" 
-    }
+  tags = {
+    Name = "ecs-sg"
+  }
 }
